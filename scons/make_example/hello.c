1e7053193b7cb159d9659d17ca7a04f4340ef89b
eggdice <eggdice@cloudera.com>
2021-01-05 12:25:13 +0100

    add materials for scons talk

---
+++ scons/make_example/hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+  printf("Hello, World, and Happy New Year!\n");
+  return 0;
+}
---
+++ scons/simple_c_example/greet.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+  printf("Hello, VIZ team!!!\n");
+  return 0;
+}
