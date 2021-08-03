4f1b03fe0258fa613c2d1504d46251173eddccb5
eggdice <eggdice@cloudera.com>
2021-08-03 12:57:18 +0200

    react

--- react-basics/demo/src/index.js
+++ react-basics/demo/src/index.js
@@ -1,6 +1,159 @@
+'use strict';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+
+class TestLifecycle extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('creation');
+    this.state = {date: new Date()};
+    this.intervalId = null;
+  }
+
+  render() {
+    console.log('render', document?.querySelector('h1')?.innerHTML);
+    return <h1>{ this.state.date.getTime() }</h1>;
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({date: new Date()});
+      console.log('tick');
+    }, 1000);
+    console.log('mount');
+  }
+
+  componentDidUpdate() {
+    console.log('update', document.querySelector('h1').innerHTML);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+}
+
+const DeleteTimer = () => {
+  const [renderTimer, setRenderTimer] = useState(true);
+
+
+
+  return (
+    <>
+      { renderTimer ? <TestLifecycle /> : <h1>Deleted</h1> }
+      <button onClick={() => setRenderTimer(false)}>Delete</button>
+    </>
+  )
+}
+
+ReactDOM.render(
+  <DeleteTimer />,
+  document.getElementById('root')
+);
+/*
+const ChangeName = () => {
+  const [nameList, setNameList] = useState(['Noemi', 'Nurbiike', 'Misi',  'Tojas']);
+
+  const messUpNames = () => {
+    setNameList(['Csabika', 'Gizi', 'Noemi', 'Lacuska', 'Tibbor']);
+  };
+
+  return <>
+    <GreetList nameList={nameList}/>
+    <button onClick={messUpNames}>Mess up names</button>
+  </>;
+};
+
+const GreetList = ({nameList}) => {
+  return (
+    <ul>
+      { nameList.map(name => <li key={name}><Greet name={name} /></li> ) }
+    </ul>
+  );
+};
+
+const Greet = ({name}) => {
+  return (
+    <h1>Hello {name}!</h1>
+  );
+};
+
+ReactDOM.render(
+  <ChangeName />,
+  document.getElementById('root')
+);
+*/
+/*
+const myName = 'Tojas';
+const appLang = 'it';
+
+const HELLO_DICT = {
+  'en': 'Hello',
+  'it': 'Ciao',
+};
+
+const Greet = ({lang, name}) => {
+  const [number, setNumber] = useState(0);
+  const [number2, setNumber2] = useState(1);
+  console.log(lang, name);
+
+  const handleClick = (e) => {
+    console.log(e);
+    setNumber((prevNumber) => prevNumber + 1);
+    setNumber2((prevNumber) => prevNumber * 2);
+  };
+
+  return (
+    <h1 data-lang={lang} onClick={handleClick}>
+      { HELLO_DICT[lang] } { name }! { number } { number2 }
+    </h1>
+    // React.createElement('h1', {}, `${ HELLO_DICT[props.lang] } ${ prop.name }!
+  );
+};
+
+class Greet2 extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = { number: 0 };
+  }
+
+  handleClick(e) {
+    console.log(e);
+    this.setState((prevState) => ({ number: prevState.number + 1 }));
+    console.log(this);
+  }
+
+  render() {
+    const { lang, name } = this.props;
+    return (
+      <h1 data-lang={lang} onClick={() => this.handleClick()}>
+        { HELLO_DICT[lang] } { name }! { this.state.number }
+      </h1>
+    );
+    // React.createElement('h1', {onClick: this.handleClick}, `${ HELLO_DICT[props.lang] } ${ prop.name }!
+  }
+}
+*/
+/*
+let o = {name: 'Jozsi', handleClick() {console.log(this.name)}};
+
+let handle = o.handleClick;
+handle();
+
+setTimeout(o.handleClick, 100);
+*/
+    /*
+ReactDOM.render(
+  <Greet name={myName} lang={appLang} />,
+  document.getElementById('root')
+);
+
+console.log(<Greet2 name={myName} lang={'en'} />);
+*/
+//React.createElement(Greet, { name: myName, lang: 'en' }, []);
+
+
 /*
 ReactDOM.render(
   <React.StrictMode>
@@ -15,6 +168,7 @@ const element = document.createElement('h1');
 element.innerText = 'Hi Viz team!';
 root.appendChild(element);
 */
+/*
 const root = document.getElementById('root');
 let name = 'Viz Team';
 let list = ['Apple', 'Melon', 'Pear'];
@@ -29,7 +183,7 @@ const element =
      { list.map(e => <li>{e}</li>) }
     </ul>
   </>
-
+*/
 /*
 React.createElement('header', {}, [
   React.createElement('h1', {apple: "4", pear: "5"}, `Hi ${name}!`),
@@ -39,7 +193,7 @@ React.createElement('header', {}, [
 React,
 */
 
-console.log(element);
+//console.log(element);
 /*
 function MyPage({name}) {
   let [number, setNumber] = React.useState(0);
@@ -58,7 +212,7 @@ function MyPage({name}) {
     </>;
 }
 */
-
+/*
 function TodoApp() {
   let [todos, setTodos] = useState(['Buy milk', 'Drink']);
   let [item, setItem] = useState('');
@@ -87,6 +241,7 @@ function TodoApp() {
 }
 
 ReactDOM.render(<TodoApp/>, root);
+*/
 // React.createElement(MyPage, {}, []);
 //
 // If you want to start measuring performance in your app, pass a function
